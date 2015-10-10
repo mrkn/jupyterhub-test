4b97417774707c138cf39da2bc954de447484ab5
Kenta Murata <mrkn@cookpad.com>
2015-10-11 03:40:51 +0900

    Use remote_file resource for docker.list

--- itamae/docker.rb
+++ itamae/docker.rb
@@ -1,9 +1,7 @@
 execute 'apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D'
 
-file "/etc/apt/sources.list.d/docker.list" do
-  content <<-END_DOCKER_LIST
-deb https://apt.dockerproject.org/repo ubuntu-trusty main
-  END_DOCKER_LIST
+remote_file '/etc/apt/sources.list.d/docker.list' do
+  source '../etc/apt/sources.list.d/docker.list'
 end
 
 execute 'apt-get update'
