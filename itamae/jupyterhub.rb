2d734cf8aa9d0760ee748eb9a8f8fe95f33d5cdd
Kenta Murata <mrkn@cookpad.com>
2015-10-11 07:33:29 +0900

    Add jupyterhub recipe
    It was forgotten to add in the commit before the last.

--- itamae/jupyterhub.rb
+++ itamae/jupyterhub.rb
@@ -7,5 +7,28 @@ end
 
 package 'python3-pip'
 
-execute 'pip3 install jupyterhub'
 execute 'pip3 install jupyter'
+
+execute 'pip3 install jupyterhub'
+
+git 'dockerspawner' do
+  repository 'https://github.com/jupyter/dockerspawner.git'
+end
+
+execute 'cd dockerspawner; pip3 install -r requirements.txt'
+
+execute 'cd dockerspawner; python3 setup.py install'
+
+directory '/etc/jupyterhub'
+
+remote_file '/etc/jupyterhub/docker_config.py' do
+  source '../etc/jupyterhub/docker_config.py'
+end
+
+execute 'docker pull jupyter/singleuser'
+execute 'docker pull jupyter/systemuser'
+
+remote_file '/home/vagrant/run.sh' do
+  source '../home/vagrant/run.sh'
+  mode '755'
+end
